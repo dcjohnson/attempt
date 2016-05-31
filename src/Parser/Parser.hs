86dc1f33069cc7b7af91b593d359806fc9ee4993
Dylan Johnson <dylanatasmsa@gmail.com>
2016-05-30 21:02:52 -0600

    more idea

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -3,10 +3,11 @@ module Parser.Parser (Parser(Parser)) where
 import Control.Monad (liftM, ap)
 import Data.Functor
 import Control.Applicative
+import Data.Tuple
 
-data Parser a = Parser (String -> [(a, String)])
+data Parser a = Parser (String -> (a, String))
 
-parse :: Parser a -> (String -> [(a, String)])
+parse :: Parser a -> (String -> (a, String))
 parse (Parser a) = a
 
 instance Functor Parser where
@@ -17,5 +18,12 @@ instance Applicative Parser where
   (<*>) = ap
     
 instance Monad Parser where
-  return a = Parser (\cs -> [(a, cs)])
-  parser >>= func = Parser (\cs -> concat [parse (func a) cs | (a, cs) <- parse parser cs])
+  return a = Parser (\cs -> (a, cs))
+  parser >>= func = Parser (\input ->
+                             let parsed = parse parser input
+                             in parse (func (fst parsed)) (snd parsed))
+
+item :: Parser String
+item = Parser (\input -> case input of
+                    ""     -> ("", "")
+                    (c:cs) -> ([c], cs))
